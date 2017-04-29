0b7eff50cfc5e4b064357f8450a4eddaddd85a14
Kaustubh Patange <DeveloperKP16@gmail.com>
2017-04-29 22:26:58 +0530

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,168 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lblFileName = New System.Windows.Forms.Label()
+        Me.lblFileSize = New System.Windows.Forms.Label()
+        Me.lblFileType = New System.Windows.Forms.Label()
+        Me.lblCreated = New System.Windows.Forms.Label()
+        Me.lblModified = New System.Windows.Forms.Label()
+        Me.pbIcon = New System.Windows.Forms.PictureBox()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.pbIcon, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.AllowDrop = True
+        Me.TextBox1.Location = New System.Drawing.Point(12, 41)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(464, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.AllowDrop = True
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(93, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(74, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "<--- Drop Here"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.pbIcon)
+        Me.GroupBox1.Controls.Add(Me.lblModified)
+        Me.GroupBox1.Controls.Add(Me.lblCreated)
+        Me.GroupBox1.Controls.Add(Me.lblFileType)
+        Me.GroupBox1.Controls.Add(Me.lblFileSize)
+        Me.GroupBox1.Controls.Add(Me.lblFileName)
+        Me.GroupBox1.Location = New System.Drawing.Point(14, 70)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(462, 124)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "File Info"
+        '
+        'lblFileName
+        '
+        Me.lblFileName.AutoSize = True
+        Me.lblFileName.Location = New System.Drawing.Point(68, 19)
+        Me.lblFileName.Name = "lblFileName"
+        Me.lblFileName.Size = New System.Drawing.Size(57, 13)
+        Me.lblFileName.TabIndex = 0
+        Me.lblFileName.Text = "File Name:"
+        '
+        'lblFileSize
+        '
+        Me.lblFileSize.AutoSize = True
+        Me.lblFileSize.Location = New System.Drawing.Point(68, 52)
+        Me.lblFileSize.Name = "lblFileSize"
+        Me.lblFileSize.Size = New System.Drawing.Size(49, 13)
+        Me.lblFileSize.TabIndex = 1
+        Me.lblFileSize.Text = "File Size:"
+        '
+        'lblFileType
+        '
+        Me.lblFileType.AutoSize = True
+        Me.lblFileType.Location = New System.Drawing.Point(68, 36)
+        Me.lblFileType.Name = "lblFileType"
+        Me.lblFileType.Size = New System.Drawing.Size(53, 13)
+        Me.lblFileType.TabIndex = 2
+        Me.lblFileType.Text = "File Type:"
+        '
+        'lblCreated
+        '
+        Me.lblCreated.AutoSize = True
+        Me.lblCreated.Location = New System.Drawing.Point(68, 83)
+        Me.lblCreated.Name = "lblCreated"
+        Me.lblCreated.Size = New System.Drawing.Size(47, 13)
+        Me.lblCreated.TabIndex = 3
+        Me.lblCreated.Text = "Created:"
+        '
+        'lblModified
+        '
+        Me.lblModified.AutoSize = True
+        Me.lblModified.Location = New System.Drawing.Point(68, 99)
+        Me.lblModified.Name = "lblModified"
+        Me.lblModified.Size = New System.Drawing.Size(50, 13)
+        Me.lblModified.TabIndex = 4
+        Me.lblModified.Text = "Modified:"
+        '
+        'pbIcon
+        '
+        Me.pbIcon.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.pbIcon.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pbIcon.Location = New System.Drawing.Point(13, 19)
+        Me.pbIcon.Name = "pbIcon"
+        Me.pbIcon.Size = New System.Drawing.Size(40, 40)
+        Me.pbIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pbIcon.TabIndex = 5
+        Me.pbIcon.TabStop = False
+        '
+        'Form1
+        '
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(490, 207)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "DrapDrop"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.pbIcon, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents lblFileSize As System.Windows.Forms.Label
+    Friend WithEvents lblFileName As System.Windows.Forms.Label
+    Friend WithEvents lblFileType As System.Windows.Forms.Label
+    Friend WithEvents lblCreated As System.Windows.Forms.Label
+    Friend WithEvents lblModified As System.Windows.Forms.Label
+    Friend WithEvents pbIcon As System.Windows.Forms.PictureBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,67 @@
+﻿' NOTE: SET BUTTON AllowDrop PROPERTY TO TRUE
+Imports System.Runtime
+Imports System.IO
+
+Public Class Form1
+    Private MyFile As FileInfo
+
+    Private Sub Button1_DragEnter(sender As Object, e As System.Windows.Forms.DragEventArgs) Handles Button1.DragEnter
+        ' DISPLAY BEHAVIOR [MOUSE ICON] FOR FILE DRAG
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        End If
+    End Sub
+
+    Private Sub Button1_DragDrop(sender As Object, e As System.Windows.Forms.DragEventArgs) Handles Button1.$safeprojectname$
+        ' GET [SINGLE] FILE PATH FROM DROPPED FILE
+        Dim file As String = e.Data.GetData(DataFormats.FileDrop)(0)
+
+        ' GET PATH & FILEINFO
+        Dim path As String = file
+        MyFile = New FileInfo(path)
+
+        ' ABORT IF NOT A VALID FILE
+        If String.IsNullOrWhiteSpace(MyFile.Extension) Then Exit Sub
+
+        ' SHOW FILE DETAILS
+        ShowFileInfo()
+
+        ' UPDATE FORM CONTROLS
+        UpdateControls()
+    End Sub
+
+    Private Sub ShowFileInfo()
+        ' DISPLAY FILE ATTRIBUTES
+        TextBox1.Text = MyFile.FullName
+
+        lblFileName.Text = "File Name: " & MyFile.Name
+        lblFileType.Text = "File Type: " & MyFile.Extension
+        lblFileSize.Text = "Size: " & Math.Round(MyFile.Length / 1024) & "KB"
+        lblCreated.Text = "Created: " & MyFile.CreationTime
+        lblModified.Text = "Modified: " & MyFile.LastWriteTime
+
+        pbIcon.Image = Icon.ExtractAssociatedIcon(MyFile.FullName).ToBitmap
+    End Sub
+
+    Private Sub UpdateControls()
+        'GET FILE NAME FROM PATH & RESIZE BUTTON TO FIT TEXT
+        Button1.Text = MyFile.Name ' Set button text to file name
+        Button1.Width = GetButtonSize(MyFile.Name) ' Use function to calulate new size
+
+        ' MOVE "DROP HERE" LABEL WITH RESIZED BUTTON
+        Label1.Left = Button1.Left + Button1.Width + 20
+    End Sub
+
+    Private Function GetButtonSize(text As String) As Integer
+        ' MEASURE SIZE OF NEW TEXT & ADD BUTTON MARGINS
+        Return Button1.CreateGraphics.MeasureString(text, Button1.Font).Width() + Button1.Margin.Left + Button1.Margin.Right + 5
+    End Function
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Try
+            ' EXECUTE OR OPEN FILE WITH DEFAULT PROGRAM
+            Process.Start(TextBox1.Text)
+        Catch ex As Exception
+        End Try
+    End Sub
+End Class
